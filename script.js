082e6cb72b4fb599ee575a1a47cd8caf9f6822c6
Kirithika-Desinger <kiruthikab865@gmail.com>
2025-07-03 21:16:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic interaction script
+// You can expand this with form validation, animations, etc.
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Portfolio loaded!");
+});
